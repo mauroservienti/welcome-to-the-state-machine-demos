993013a6526829666980594268132479aab427bc
Mauro Servienti <mauro.servienti@gmail.com>
2019-05-02 07:37:27 +0200

    NServiceBus hosting and DI shared infrastructure

---
+++ src/NServiceBus.Shared.Hosting/SessionAndConfigurationHolder.cs
@@ -0,0 +1,24 @@
+﻿namespace NServiceBus.Shared.Hosting
+{
+    class SessionAndConfigurationHolder
+    {
+        public SessionAndConfigurationHolder(EndpointConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IMessageSession Session { get; private set; }
+
+        public EndpointConfiguration Configuration { get; }
+
+        public void OnStart(IMessageSession session)
+        {
+            Session = session;
+        }
+
+        public void OnStop()
+        {
+            Session = null;
+        }
+    }
+}
---
+++ src/NServiceBus.Shared.MicrosoftDependencyInjection/MicrosoftDependencyInjection.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using NServiceBus.ObjectBuilder.MSDependencyInjection;
+using System;
+
+namespace NServiceBus.Shared.MicrosoftDependencyInjection
+{
+    public static class MicrosoftDependencyInjection
+    {
+        public static IServiceProvider UseMicrosoftDependencyInjection(this EndpointConfiguration endpointConfiguration, IServiceCollection services)
+        {
+            UpdateableServiceProvider container = null;
+            endpointConfiguration.UseContainer<ServicesBuilder>(c =>
+            {
+                c.ExistingServices(services);
+                c.ServiceProviderFactory(sc =>
+                {
+                    container = new UpdateableServiceProvider(services);
+                    return container;
+                });
+            });
+
+            return container;
+        }
+    }
+}
