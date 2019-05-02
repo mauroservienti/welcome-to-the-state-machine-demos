e6c600330619a308e598b9dd91b4d91ee665e2f9
Mauro Servienti <mauro.servienti@gmail.com>
2019-05-02 07:37:59 +0200

    Finance service skeleton

---
+++ src/Finance.Api/Startup.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using NServiceBus.Shared.Hosting;
+using System;
+using NServiceBus;
+using NServiceBus.Shared.MicrosoftDependencyInjection;
+
+namespace Finance.Api
+{
+    public class Startup
+    {
+        public IServiceProvider ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAllOrigins", builder =>
+                {
+                    builder.AllowAnyOrigin();
+                });
+            });
+
+            services.AddSignalR();
+            services.AddMvc()
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            var serviceProvider = services.AddNServiceBus("Finance.Api", endpointConfiguration =>
+            {
+                endpointConfiguration.ApplyCommonConfiguration(asSendOnly: true);
+                return endpointConfiguration.UseMicrosoftDependencyInjection(services);
+            });
+
+            return serviceProvider;
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseCors("AllowAllOrigins");
+            app.UseMvc();
+        }
+    }
+}
