cbd9fd01c1c223bd7083fdd95bc42319c29e5fe8
mauroservienti <mauro.servienti@gmail.com>
2019-05-09 15:15:38 +0200

    Add Finance Service tests

---
+++ src/Finance.Service.Tests/When_order_is_checked_out.cs
@@ -0,0 +1,67 @@
+using FakeItEasy;
+using Finance.Service.Messages;
+using Finance.Service.Policies;
+using NServiceBus.Testing;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Ticketing.Messages.Events;
+using Xunit;
+
+namespace Finance.Service.Tests
+{
+    public class When_order_is_checked_out
+    {
+        [Fact]
+        public async Task Payment_policy_is_expected_state()
+        {
+            // arrange
+            var saga = new PaymentPolicy
+            {
+                Data = new PaymentPolicyState()
+            };
+
+            var orderCheckedOut = A.Fake<IOrderCheckedOut>();
+
+            A.CallTo(() => orderCheckedOut.CustomerId).Returns(Guid.NewGuid());
+            A.CallTo(() => orderCheckedOut.OrderId).Returns(Guid.NewGuid());
+
+            // act
+            await saga.Handle(orderCheckedOut, new TestableMessageHandlerContext());
+
+            // assert
+            Assert.Equal(orderCheckedOut.OrderId, saga.Data.OrderId);
+            Assert.Equal(orderCheckedOut.CustomerId, saga.Data.CustomerId);
+            Assert.False(saga.Data.CardAuthorized);
+            Assert.False(saga.Data.CardCharged);
+        }
+
+        [Fact]
+        public async Task Card_authorization_process_has_been_requested()
+        {
+            // arrange
+            var saga = new PaymentPolicy
+            {
+                Data = new PaymentPolicyState()
+            };
+            var context = new TestableMessageHandlerContext();
+
+            var orderCheckedOut = A.Fake<IOrderCheckedOut>();
+
+            A.CallTo(() => orderCheckedOut.CustomerId).Returns(Guid.NewGuid());
+            A.CallTo(() => orderCheckedOut.OrderId).Returns(Guid.NewGuid());
+
+            // act
+            await saga.Handle(orderCheckedOut, context);
+
+            // assert
+            var authorizeCardMessage = context.SentMessages
+                .Select(sm => sm.Message)
+                .OfType<AuthorizeCard>()
+                .SingleOrDefault();
+
+            Assert.NotNull(authorizeCardMessage);
+            Assert.Equal(authorizeCardMessage.OrderId, saga.Data.OrderId);
+        }
+    }
+}
