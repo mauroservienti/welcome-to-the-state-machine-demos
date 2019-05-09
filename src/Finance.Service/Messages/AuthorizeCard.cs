e186ebd7644cd0e7e7f7123deb640489c866c377
mauroservienti <mauro.servienti@gmail.com>
2019-05-09 15:15:30 +0200

    Finance Payment Policy initial draft

---
+++ src/Finance.Service/Messages/AuthorizeCard.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Finance.Service.Messages
+{
+    class AuthorizeCard
+    {
+        public Guid OrderId { get; set; }
+    }
+}
---
+++ src/Finance.Service/Policies/PaymentPolicy.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using Finance.Service.Messages;
+using NServiceBus;
+using Ticketing.Messages.Events;
+
+namespace Finance.Service.Policies
+{
+    class PaymentPolicy : Saga<PaymentPolicyState>,
+        IAmStartedByMessages<IOrderCheckedOut>
+    {
+        public Task Handle(IOrderCheckedOut message, IMessageHandlerContext context)
+        {
+            Data.OrderId = message.OrderId;
+            Data.CustomerId = message.CustomerId;
+
+            return context.SendLocal(new AuthorizeCard() { OrderId = Data.OrderId });
+        }
+
+        protected override void ConfigureHowToFindSaga(SagaPropertyMapper<PaymentPolicyState> mapper)
+        {
+            mapper.ConfigureMapping<IOrderCheckedOut>(m => m.OrderId).ToSaga(s => s.OrderId);
+        }
+    }
+
+    class PaymentPolicyState : ContainSagaData
+    {
+        public Guid OrderId{ get; set; }
+        public Guid CustomerId { get; set; }
+        public bool CardCharged { get; internal set; }
+        public bool CardAuthorized { get; internal set; }
+    }
+}
