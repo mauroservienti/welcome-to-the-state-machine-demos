93ee9f31dfa745acda9fa1ac67a8aad9ec52eb9d
mauroservienti <mauro.servienti@gmail.com>
2019-05-09 18:03:06 +0200

    Payment Gateway messages

---
+++ src/Finance.PaymentGateway.Messages/AuthorizeCard.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Finance.PaymentGateway.Messages
+{
+    public class AuthorizeCard
+    {
+        public Guid OrderId { get; set; }
+    }
+}
---
+++ src/Finance.PaymentGateway.Messages/AuthorizeCardResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Finance.PaymentGateway.Messages
+{
+    public class AuthorizeCardResponse
+    {
+        public bool Succeeded { get; set; }
+        public Guid OrderId { get; set; }
+    }
+}
