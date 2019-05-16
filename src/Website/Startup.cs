213d3043a26aab6d6532f8895384e6f3bd8e0bae
Mauro Servienti <mauro.servienti@gmail.com>
2019-05-16 08:20:59 +0200

    basic webapp

---
+++ src/Website/Startup.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NServiceBus;
+using ServiceComposer.AspNetCore;
+using ServiceComposer.AspNetCore.Mvc;
+using ITOps.UIComposition.Mvc;
+
+namespace Website
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public IServiceProvider ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            services.AddMvc()
+                .AddUIComposition()
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddViewModelComposition(options =>
+            {
+                options.AddMvcSupport();
+            });
+
+            return services.AddNServiceBus("Webapp", endpointConfiguration =>
+            {
+                endpointConfiguration.ApplyCommonConfiguration(asSendOnly: true);
+                return endpointConfiguration.UseMicrosoftDependencyInjection(services);
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
