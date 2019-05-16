64df517250c415c04061ff9e00c56a4214e79557
Mauro Servienti <mauro.servienti@gmail.com>
2019-05-16 08:20:44 +0200

    Ticketing ViewComponents

---
+++ src/Ticketing.ViewComponents/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Ticketing.ViewComponents.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
