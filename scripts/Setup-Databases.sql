8ebfe3d12260f706f359209c85273c20144cae04
mauroservienti <mauro.servienti@gmail.com>
2019-05-16 15:08:09 +0200

    Ticketing DB scripts

--- scripts/Setup-Databases.sql
+++ scripts/Setup-Databases.sql
@@ -1,2 +1,6 @@
 USE [master]
 GO
+
+IF  NOT EXISTS (SELECT name FROM master.dbo.sysdatabases WHERE name = N'Tickets')
+CREATE DATABASE [Sales]
+GO
--- scripts/Teardown-Databases.sql
+++ scripts/Teardown-Databases.sql
@@ -1,2 +1,6 @@
 USE [master]
 GO
+
+IF EXISTS (SELECT name FROM master.dbo.sysdatabases WHERE name = N'Tickets')
+DROP DATABASE [Sales]
+GO
