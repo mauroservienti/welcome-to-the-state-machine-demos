7db23ad29bb12ac20a24f120507a982532e42e36
mauroservienti <mauro.servienti@gmail.com>
2019-05-17 16:55:39 +0200

    Finance Ticket Price composition

---
+++ src/Finance.ViewComponents/TicketPriceViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Finance.ViewComponents
+{
+    [ViewComponent(Name = "Finance.ViewComponents.TicketPrice")]
+    public class TicketPriceViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(dynamic viewModel)
+        {
+            return View(viewModel);
+        }
+    }
+}
