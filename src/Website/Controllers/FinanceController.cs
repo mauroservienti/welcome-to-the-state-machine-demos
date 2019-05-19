01a0bcd835c9b42fd2abc23efda78eef279bfe14
Mauro Servienti <mauro.servienti@gmail.com>
2019-05-19 16:27:50 +0200

    select payment method action/controller

---
+++ src/Website/Controllers/FinanceController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Website.Controllers
+{
+    public class FinanceController : Controller
+    {
+        public IActionResult SelectPaymentMethod()
+        {
+            return RedirectToAction("Review", "Reservations");
+        }
+    }
+}
