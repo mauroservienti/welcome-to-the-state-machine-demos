fbee5645e64d0b7b5efc489eb967c3c862a41e59
Mauro Servienti <mauro.servienti@gmail.com>
2019-05-19 05:55:29 +0200

    ViewModel Composition handles tickets reservations

--- src/Reservations.ViewComponents/Controllers/ReservationsController.cs
+++ src/Reservations.ViewComponents/Controllers/ReservationsController.cs
@@ -1,36 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using NServiceBus;
-using Reservations.Messages.Commands;
-using System;
-using System.Threading.Tasks;
 
 namespace Reservations.ViewComponents.Controllers
 {
     public class ReservationsController : Controller
     {
-        private readonly IMessageSession messageSession;
-
-        public ReservationsController(IMessageSession messageSession)
-        {
-            this.messageSession = messageSession;
-        }
-
         public IActionResult Index()
         {
             return View();
         }
 
         [HttpPost]
-        public async Task<IActionResult> Reserve(int id)
+        public IActionResult Reserve(int id)
         {
-            var message = new ReserveTicket()
-            {
-                TicketId = id,
-                ReservationId = new Guid(ControllerContext.HttpContext.Request.Cookies["reservation-id"])
-            };
-            //WARN: destination is hardcoded to reduce demo complexity. In a real project it should not.
-            await messageSession.Send("Reservations.Service", message);
-
             return RedirectToAction("Added");
         }
 
