96a91f947748c5d5d0b1780564ea32ef7d3a3a3b
Mauro Servienti <mauro.servienti@gmail.com>
2019-05-19 16:23:45 +0200

    handle payment method selection

---
+++ src/Finance.ViewModelComposition/SelectPaymentMethodPostHandler.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Finance.Data;
+using Finance.Data.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.EntityFrameworkCore;
+using NServiceBus;
+using ServiceComposer.AspNetCore;
+
+namespace Finance.ViewModelComposition
+{
+    class SelectPaymentMethodPostHandler : IHandleRequests
+    {
+        public bool Matches(RouteData routeData, string httpVerb, HttpRequest request)
+        {
+            var controller = (string)routeData.Values["controller"];
+            var action = (string)routeData.Values["action"];
+
+            return HttpMethods.IsPost(httpVerb)
+                   && controller.ToLowerInvariant() == "finance"
+                   && action.ToLowerInvariant() == "selectpaymentmethod";
+        }
+
+        public async Task Handle(string requestId, dynamic vm, RouteData routeData, HttpRequest request)
+        {
+            var reservationId = new Guid(request.Cookies["reservation-id"]);
+            using (var db = FinanceContext.Create())
+            {
+                var reservationPaymentMethod = await db.ReservationsPaymentMethod
+                    .Where(rpm => rpm.Id == reservationId)
+                    .SingleOrDefaultAsync();
+
+                if (reservationPaymentMethod == null)
+                {
+                    reservationPaymentMethod = new ReservationPaymentMethod()
+                    {
+                        Id = reservationId
+                    };
+                    db.ReservationsPaymentMethod.Add(reservationPaymentMethod);
+                }
+
+                reservationPaymentMethod.PaymentMethod = int.Parse(request.Form["PaymentMethod"]);
+
+                await db.SaveChangesAsync();
+            }
+        }
+    }
+}
