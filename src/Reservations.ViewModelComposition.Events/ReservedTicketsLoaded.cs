39dd2d7f0daebd7852e00493f035b53aae932b0b
Mauro Servienti <mauro.servienti@gmail.com>
2019-05-19 06:00:05 +0200

    list my reservations

---
+++ src/Reservations.ViewModelComposition.Events/ReservedTicketsLoaded.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Reservations.ViewModelComposition.Events
+{
+    public class ReservedTicketsLoaded
+    {
+        public IDictionary<int, dynamic> ReservedTicketsViewModel { get; set; }
+        public dynamic Reservation { get; set; }
+    }
+}
