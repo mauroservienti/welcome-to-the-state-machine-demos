2a50e8e707ec304a170bccd7b545399718a5fcdb
Mauro Servienti <mauro.servienti@gmail.com>
2019-05-19 05:57:49 +0200

    eval tickets left on the fly

--- src/Reservations.ViewModelComposition/AvailableTicketsLoadedSubscriber.cs
+++ src/Reservations.ViewModelComposition/AvailableTicketsLoadedSubscriber.cs
@@ -2,6 +2,8 @@
 using Microsoft.AspNetCore.Routing;
 using Microsoft.EntityFrameworkCore;
 using ServiceComposer.AspNetCore;
+using System;
+using System.Collections.Generic;
 using System.Linq;
 using Ticketing.ViewModelComposition.Events;
 
@@ -22,18 +24,41 @@ namespace Reservations.ViewModelComposition
 
         public void Subscribe(IPublishCompositionEvents publisher)
         {
-            publisher.Subscribe<AvailableTicketsLoaded>(async (requestId, pageViewModel, @event, rd, req) =>
+            publisher.Subscribe<AvailableTicketsLoaded>(async (requestId, viewModel, @event, routeData, httpRequest) =>
             {
                 var ids = @event.AvailableTicketsViewModel.Keys.ToArray();
                 using (var db = Data.ReservationsContext.Create())
                 {
                     var availableTickets = await db.AvailableTickets
-                        .Where(ticketPrice => ids.Contains(ticketPrice.Id))
+                        .Where(ticket => ids.Contains(ticket.Id))
                         .ToListAsync();
+
+                    IDictionary<int, int> reservedTickets = new Dictionary<int, int>();
+
+                    if (httpRequest.Cookies.ContainsKey("reservation-id"))
+                    {
+                        var reservationId = new Guid(httpRequest.Cookies["reservation-id"]);
+                        var reservation = await db.Reservations
+                            .Where(r => r.Id == reservationId)
+                            .Include(r => r.ReservedTickets)
+                            .SingleOrDefaultAsync();
+
+                        if (reservation != null)
+                        {
+                            reservedTickets = reservation.ReservedTickets
+                                .GroupBy(t => t.TicketId)
+                                .ToDictionary(g => g.Key, g => g.Count());
+                        }
+                    }
 
                     foreach (var availableTicket in availableTickets)
                     {
-                        @event.AvailableTicketsViewModel[(int)availableTicket.Id].TicketsLeft = availableTicket.TicketsLeft;
+                        var avialableTicketId = (int)availableTicket.Id;
+                        var reservedQuantity = reservedTickets.ContainsKey(avialableTicketId)
+                            ? reservedTickets[avialableTicketId]
+                            : 0;
+
+                        @event.AvailableTicketsViewModel[avialableTicketId].TicketsLeft = availableTicket.TotalTickets - reservedQuantity;
                     }
                 }
             });
