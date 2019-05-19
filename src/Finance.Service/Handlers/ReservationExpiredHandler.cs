eb6261fcbfe4f3bb466ccfcab5d2a798f5fba608
Mauro Servienti <mauro.servienti@gmail.com>
2019-05-19 07:20:52 +0200

    handle expired reservations

---
+++ src/Finance.Service/Handlers/ReservationExpiredHandler.cs
@@ -0,0 +1,28 @@
+﻿using Finance.Data;
+using Microsoft.EntityFrameworkCore;
+using NServiceBus;
+using Reservations.Messages.Events;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Finance.Service.Handlers
+{
+    class ReservationExpiredHandler : IHandleMessages<IReservationExpired>
+    {
+        public async Task Handle(IReservationExpired message, IMessageHandlerContext context)
+        {
+            using (var db = FinanceContext.Create())
+            {
+                //very inefficient way of deleting stuff.
+                var reservations = await db.ReservedTickets
+                    .Where(r => r.ReservationId == message.ReservationId)
+                    .ToListAsync();
+                if (reservations.Any())
+                {
+                    db.RemoveRange(reservations);
+                    await db.SaveChangesAsync();
+                }
+            }
+        }
+    }
+}
