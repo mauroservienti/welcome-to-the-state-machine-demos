1eb2b2ff593f879a8dd42eacbed6b774960b0fd8
Mauro Servienti <mauro.servienti@gmail.com>
2019-05-19 08:32:54 +0200

    Move reservation middleware to its owner

--- src/Reservations.ViewModelComposition/Middlewares/ReservationMiddleware.cs
+++ src/Reservations.ViewModelComposition/Middlewares/ReservationMiddleware.cs
@@ -2,7 +2,7 @@
 using System;
 using System.Threading.Tasks;
 
-namespace ITOps.Middlewares
+namespace Reservations.ViewModelComposition.Middlewares
 {
     public class ReservationMiddleware
     {
--- src/Website/Startup.cs
+++ src/Website/Startup.cs
@@ -1,14 +1,13 @@
 ﻿using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using NServiceBus;
 using ServiceComposer.AspNetCore;
 using ServiceComposer.AspNetCore.Mvc;
-using ITOps.Middlewares;
+using Reservations.ViewModelComposition.Middlewares;
 
 namespace Website
 {
