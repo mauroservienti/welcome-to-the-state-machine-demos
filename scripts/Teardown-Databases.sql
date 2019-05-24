1d0629da4fafaa01c2aff158063e7561536e9dc6
Mauro Servienti <mauro.servienti@gmail.com>
2019-05-24 19:06:07 +0200

    Sales orders data

--- scripts/Teardown-Databases.sql
+++ scripts/Teardown-Databases.sql
@@ -12,3 +12,7 @@ GO
 IF EXISTS (SELECT name FROM master.dbo.sysdatabases WHERE name = N'Reservations')
 DROP DATABASE [Reservations]
 GO
+
+IF EXISTS (SELECT name FROM master.dbo.sysdatabases WHERE name = N'Sales')
+DROP DATABASE [Sales]
+GO
