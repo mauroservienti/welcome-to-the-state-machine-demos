9bb8cf0635a5d107c494b7c164930191dad02d6d
Mauro Servienti <mauro.servienti@gmail.com>
2019-05-25 09:47:16 +0200

    initialize the Order policy when the Order is created

---
+++ src/Sales.Service/Policies/OrderPolicy.cs
@@ -0,0 +1,26 @@
+﻿using NServiceBus;
+using Sales.Messages.Events;
+using System;
+using System.Threading.Tasks;
+
+namespace Sales.Service.Policies
+{
+    class OrderPolicy : Saga<OrderPolicy.State>,
+        IAmStartedByMessages<IOrderCreated>
+    {
+        public class State : ContainSagaData
+        {
+            public Guid OrderId { get; set; }
+        }
+
+        protected override void ConfigureHowToFindSaga(SagaPropertyMapper<State> mapper)
+        {
+            mapper.ConfigureMapping<IOrderCreated>(m => m.OrderId).ToSaga(s => s.OrderId);
+        }
+
+        public Task Handle(IOrderCreated message, IMessageHandlerContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
