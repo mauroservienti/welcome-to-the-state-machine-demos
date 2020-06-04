2f81aaeeb91bafe90153bb8fdf894a4be87b0a90
mauroservienti <mauro.servienti@gmail.com>
2020-06-04 14:25:00 +0200

    Empty tests project

---
+++ src/Choreography.Tests/UnitTest1.cs
@@ -0,0 +1,25 @@
+using NUnit.Framework;
+
+namespace Choreography.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
