7a457f39fa99372e6d25d76ae3af26c90de05e9d
mauroservienti <mauro.servienti@gmail.com>
2020-12-10 09:15:55 +0100

    not used using statements

--- src/Finance.ViewModelComposition/ReviewReservedTicketsLoadedSubscriber.cs
+++ src/Finance.ViewModelComposition/ReviewReservedTicketsLoadedSubscriber.cs
@@ -1,6 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Routing;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using Reservations.ViewModelComposition.Events;
 using ServiceComposer.AspNetCore;
 using System;
