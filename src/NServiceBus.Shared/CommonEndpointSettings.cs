0f8efc90b9b693d80c13798f168a8bb7da7b94e6
Mauro Servienti <mauro.servienti@gmail.com>
2022-03-25 09:58:41 +0100

    send only is never used

--- src/NServiceBus.Shared/CommonEndpointSettings.cs
+++ src/NServiceBus.Shared/CommonEndpointSettings.cs
@@ -4,16 +4,13 @@ namespace NServiceBus
 {
     public static class CommonEndpointSettings
     {
-        public static void ApplyCommonConfiguration(this EndpointConfiguration endpointConfiguration, bool asSendOnly = false, Action<RoutingSettings<LearningTransport>> configureRouting = null)
+        public static void ApplyCommonConfiguration(this EndpointConfiguration endpointConfiguration, Action<RoutingSettings<LearningTransport>> configureRouting = null)
         {
             endpointConfiguration.UseSerialization<NewtonsoftSerializer>();
             var transportConfig = endpointConfiguration.UseTransport<LearningTransport>();
             configureRouting?.Invoke(transportConfig.Routing());
 
-            if (!asSendOnly)
-            {
-                endpointConfiguration.UsePersistence<LearningPersistence>();
-            }
+            endpointConfiguration.UsePersistence<LearningPersistence>();
 
             endpointConfiguration.AuditProcessedMessagesTo("audit");
             endpointConfiguration.SendFailedMessagesTo("error");
@@ -31,17 +28,10 @@ namespace NServiceBus
             messageConventions.DefiningEventsAs(t => t.Namespace != null && t.Namespace.EndsWith(".Messages.Events"));
             messageConventions.DefiningCommandsAs(t => t.Namespace != null && t.Namespace.EndsWith(".Messages.Commands"));
 
-            if (asSendOnly)
-            {
-                endpointConfiguration.SendOnly();
-            }
-            else
-            {
-                var metrics = endpointConfiguration.EnableMetrics();
-                metrics.SendMetricDataToServiceControl(
-                    serviceControlMetricsAddress: "Particular.Monitoring",
-                    interval: TimeSpan.FromSeconds(5));
-            }
+            var metrics = endpointConfiguration.EnableMetrics();
+            metrics.SendMetricDataToServiceControl(
+                serviceControlMetricsAddress: "Particular.Monitoring",
+                interval: TimeSpan.FromSeconds(5));
         }
     }
 }
