1644f6420d89deac76492ce9c33c70b3d1eead32
Mauro Servienti <mauro.servienti@gmail.com>
2022-03-25 10:09:53 +0100

    Create a database for Shipping

--- scripts/Setup-Databases.sql
+++ scripts/Setup-Databases.sql
@@ -15,3 +15,8 @@ GO
 IF  NOT EXISTS (SELECT name FROM master.dbo.sysdatabases WHERE name = '$(DatabaseName)')
 CREATE DATABASE [$(DatabaseName)] ON ( NAME = '$(DatabaseName)', FILENAME = '$(UserPath)\$(DatabaseName).mdf' )
 GO
+
+:setvar DatabaseName "Shipping"
+IF  NOT EXISTS (SELECT name FROM master.dbo.sysdatabases WHERE name = '$(DatabaseName)')
+CREATE DATABASE [$(DatabaseName)] ON ( NAME = '$(DatabaseName)', FILENAME = '$(UserPath)\$(DatabaseName).mdf' )
+GO
