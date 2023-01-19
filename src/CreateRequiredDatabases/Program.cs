7d5911e6c9cc30b1fcf75a7ae07a6b5c003773a8
Mauro Servienti <mauro.servienti@gmail.com>
2023-01-19 10:45:58 +0100

    Database creation support

---
+++ src/CreateRequiredDatabases/Program.cs
@@ -0,0 +1,30 @@
+﻿using Finance.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
+using Reservations.Data;
+using Ticketing.Data;
+
+/*
+ * Entity Framework .Database.EnsureCreated() works only if the database is empty
+ * in this demo NServiceBus persistence might populate the database _before_ EF
+ * checks for te first time. If that happens EF never creates the db. Thus, we create
+ * it while building the solution. Another option (better, but far more complex
+ * for this demo) would have been to use migrations.
+ */
+
+void CreateRequiredTables(DbContext context)
+{
+    var databaseCreator = (RelationalDatabaseCreator)context.Database.GetService<IDatabaseCreator>();
+
+    if (databaseCreator.HasTables())
+    {
+        return;
+    }
+    
+    databaseCreator.CreateTables();
+}
+
+CreateRequiredTables(new TicketingContext());
+CreateRequiredTables(new FinanceContext());
+CreateRequiredTables(new ReservationsContext());
